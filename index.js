46b4232c6ca30d77b56061b0cd946a490ac13961
NandhiniKarvendhan <nandhukannan10@gmail.com>
2022-09-17 07:46:57 +0530

    Adding input field

--- index.js
+++ index.js
@@ -1,27 +1,44 @@
-function App(){
+function App() {
   const [todos, setTodos] = React.useState([
     {
-      text: 'learn react',
+      text: "learn react",
       isCompleted: false,
     },
     {
-      text: 'meet friend for lunch',
+      text: "meet friend for lunch",
       isCompleted: false,
     },
     {
-      text: 'build todo app',
+      text: "build todo app",
       isCompleted: false,
-    }        
-  ])
-
-  return(
+    },
+  ]);
+  const [value, setValue] = React.useState("");
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!value) return;
+    const newTodos = [...todos, { text: value, isCompleted: false }];
+    setTodos(newTodos);
+    setValue("");
+  };
+  return (
     <>
-      {todos.map((todo, i) => <div key={i}>{todo.text}</div>)}
+      {todos.map((todo, i) => (
+        <div className="todo" key={i}>
+          {todo.text}
+        </div>
+      ))}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          className="input"
+          value={value}
+          placeholder="Add Todo ..."
+          onChange={(e) => setValue(e.target.value)}
+        />
+      </form>
     </>
   );
 }
 
-ReactDOM.render(
-  <App/>,
-  document.getElementById('root')
-);
+ReactDOM.render(<App />, document.getElementById("root"));
